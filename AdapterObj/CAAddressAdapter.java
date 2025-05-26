56ecc216fae3cd836c44094ced4a91bff2b77865
FabiusC <fabioandrescar@gmail.com>
2025-05-26 07:21:40 -0500

    Facade

---
+++ AdapterObj/CAAddressAdapter.java
@@ -0,0 +1,15 @@
+public class CAAddressAdapter {
+
+  private CAAddress objCAAddress;
+
+  public CAAddressAdapter(CAAddress address) {
+    objCAAddress = address;
+  }
+
+  public boolean isValidAddress(String inp_address,
+      String inp_zip, String inp_state) {
+
+    return objCAAddress.isValidCanadianAddr(inp_address,
+        inp_zip, inp_state);
+  }
+}// end of class
